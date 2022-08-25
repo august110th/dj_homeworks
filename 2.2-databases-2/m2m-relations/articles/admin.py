5c05d7f174e349d15a0df0c6e49196f2022bed8f
august110th <august110th@yandex.ru>
2022-08-25 10:28:24 +0400

    edit admin

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
 from .models import Article, Scope, ArticleScope
+
+
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super(ArticleScopeInlineFormset, self).clean()
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                count += 1
+            if count > 1:
+                raise ValidationError('Может быть только 1 базовый тег')
+            elif count < 1:
+                raise ValidationError('Тут всегда ошибка')
+            return super().clean()  # вызываем базовый код переопределяемого метода
 
 
 class ArticleScopeInline(admin.TabularInline):
     model = ArticleScope
     extra = 1
+    formset = ArticleScopeInlineFormset
 
 
 @admin.register(Article)
@@ -15,4 +33,3 @@ class ArticleAdmin(admin.ModelAdmin):
 @admin.register(Scope)
 class ScopeAdmin(admin.ModelAdmin):
     inlines = [ArticleScopeInline]
-
