3ccad072061aa532598725ce22e2a19f1d0afcf0
august110th <august110th@yandex.ru>
2022-09-02 16:18:42 +0400

    edit serializers.py

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
+from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'stocks']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
+
     # настройте сериализатор для позиции продукта на складе
     pass
 
